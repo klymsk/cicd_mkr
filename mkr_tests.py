18a82e77193bf0cc939b58e9cf829640990f1e10
klymsk <klymsashka@gmail.com>
2025-03-28 16:12:10 +0200

    add sort tests

--- mkr_tests.py
+++ mkr_tests.py
@@ -9,3 +9,15 @@ def data_test():
         {'country': 'Germany', 'area': 500000, 'population': 70000000},
         {'country': 'UK', 'area': 300000, 'population': 50000000}
     ]
+
+def test_sorted_area(data_test):
+    sorted_data = sortArea(data_test)
+    assert sorted_data[0]['country'] == 'Ukraine', f"Вхідне: 'Ukraine', фактичне: {sorted_data[0]['country']}"
+    assert sorted_data[-1]['country'] == 'UK', f"Вхідне: 'UK', фактичне: {sorted_data[-1]['country']}"
+
+def test_sorted_population(data_test):
+    sorted_data = sortPopulation(data_test)
+    assert sorted_data[0]['country'] == 'Germany', f"Вхідне: 'Germany', фактичне: {sorted_data[0]['country']}"
+    assert sorted_data[-1]['country'] == 'Poland', f"Вхідне: 'Poland', фактичне: {sorted_data[-1]['country']}"
+
+
